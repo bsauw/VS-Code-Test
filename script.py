46920628122c0293e6b53ec6ca75aeecbf2cb3c5
Brandon Sauw <bsauw@Brandons-MBP.home>
2020-04-15 21:18:58 -0700

    removed greet function

--- script.py
+++ script.py
@@ -1,16 +1,4 @@
 import requests
-import sys
-import math
-from os import rename
-
-
-# print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = 'Hello,{}'.format(who_to_greet)
-    return greeting
 
 
 r = requests.get('https://google.com')
